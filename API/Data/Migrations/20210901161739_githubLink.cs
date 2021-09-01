eb82a3a05df822e99ee9fc4166c6eef82cc759e6
Matthew Esposito <matthew_esposito@hcl.com>
2021-09-01 14:36:46 -0400

    Added github link for profiles + profile wording

---
+++ API/Data/Migrations/20210901161739_githubLink.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Data.Migrations
+{
+    public partial class githubLink : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "githubLink",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "githubLink",
+                table: "AspNetUsers");
+        }
+    }
+}
